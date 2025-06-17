lune-climate/eslint-config

commit 45618c56ea53b545cfc0a3a3353cd1862c737fd4

Fix a few regressions (#9)
Turns out eslint-config-standard which I removed in [1] actually had a
few bits that we'd like to keep making use of:

1. Disabling some overly strict recommended ESLint rules
2. Enabling a bunch of useful n (node) rules (the peer dependency
   documents that the plugin is required)

[1] 9dbd54b82fcf ("Get rid of eslint-config-standard (#7)")

index.js
@@ -26,10 +26,19 @@ module.exports = {
         'no-use-before-define': 'off', // Unnecessary with TS and generates false positives.
         'comma-dangle': ['error', 'always-multiline'],
         'func-style': ['error', 'declaration'],
+
+        // Allows idiomatic infinite loops, rejects other cases
+        'no-constant-condition': ['error', { checkLoops: false }],
+
         'no-useless-constructor': 'off',
         'no-unused-vars': 'off',
         'no-extra-semi': 'off',
         'no-trailing-spaces': 'error',
+
+        // Doesn't complain when we have let {a, b} = obj and b is not reassigned,
+        // complains otherwise.
+        'prefer-const': ['error', { destructuring: 'all' }],
+
         '@typescript-eslint/no-useless-constructor': ['error'],
         '@typescript-eslint/no-unnecessary-type-assertion': ['error'],
         '@typescript-eslint/no-unused-vars': [
@@ -76,5 +85,13 @@ module.exports = {
         /*
         End of @typescript-eslint/recommended-type-checked exceptions
         */
+
+        'n/handle-callback-err': ['error', '^(err|error)$'],
+        'n/no-callback-literal': 'error',
+        'n/no-deprecated-api': 'error',
+        'n/no-exports-assign': 'error',
+        'n/no-new-require': 'error',
+        'n/no-path-concat': 'error',
+        'n/process-exit-as-throw': 'error',
     },
 }

commit 073d4badb121e19afbdfcee219700be1bcc6facd

Enable the n plugin
I made a mistake in an earlier commit where I added a bunch of n/...
rules but I forgot to include the plugin so that the rules are actually
available.

That resulted in a bunch of errors like

    /lune/backend/src/utils.ts
      1:1  error  Definition for rule 'n/handle-callback-err' was not found    n/handle-callback-err
      1:1  error  Definition for rule 'n/no-callback-literal' was not found    n/no-callback-literal
      1:1  error  Definition for rule 'n/no-deprecated-api' was not found      n/no-deprecated-api
      1:1  error  Definition for rule 'n/no-exports-assign' was not found      n/no-exports-assign
      1:1  error  Definition for rule 'n/no-new-require' was not found         n/no-new-require
      1:1  error  Definition for rule 'n/no-path-concat' was not found         n/no-path-concat
      1:1  error  Definition for rule 'n/process-exit-as-throw' was not found  n/process-exit-as-throw

when I attempted to use the configuration.

Fixes: 45618c56ea53 ("Fix a few regressions (#9)")

index.js
@@ -18,7 +18,11 @@ module.exports = {
         tsconfigRootDir: __dirname,
         project: ['./tsconfig.json'],
     },
-    plugins: ['@typescript-eslint', 'simple-import-sort'],
+    plugins: [
+        '@typescript-eslint',
+        'n',
+        'simple-import-sort',
+    ],
     rules: {
         // Unnecessary with TS and generates false positives.
         // https://github.com/typescript-eslint/typescript-eslint/blob/181e705887e9e07f0fa28195644cc94e5b4f039d/docs/linting/Troubleshooting.mdx#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors