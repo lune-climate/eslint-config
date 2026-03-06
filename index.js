710a107033867f6fe082511aa07c3cd8ebaf8d35
Jakub Stasiak <jakub@stasiak.at>
2026-03-06 11:07:35 +0100

    Migrate to ESLint flat config (#18)
    Flat config is available in ESLint 8.x and default in 9.x. Let's
    future-proof our ESLint configuration.
    
    The configuration is functionally exactly the same as before, only
    the format changed.
    
    Some implementation details:
    
    - Replaced @typescript-eslint/eslint-plugin peer dep with
      typescript-eslint, the unified package that exports flat config objects
      (tseslint.config(), tseslint.configs.recommendedTypeChecked, etc.)
    - Added @eslint/js dependency (programmatic replacement for "eslint:recommended")
    - Added globals dependency (replacement for env: { browser, es2021 })
    
    This is a breaking change: consumers must switch to flat config and
    install typescript-eslint instead of @typescript-eslint/eslint-plugin.
    
    Disclosure: the configuration has been LLM-translated but I then
    LLM-reviewed it multiple times and spot-checked manually.
    
    [1] https://eslint.org/docs/latest/extend/plugin-migration-flat-config

--- index.js
+++ index.js
@@ -1,92 +1,105 @@
-module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+const eslint = require('@eslint/js')
+const tseslint = require('typescript-eslint')
+const pluginPromise = require('eslint-plugin-promise')
+const pluginN = require('eslint-plugin-n')
+const pluginSimpleImportSort = require('eslint-plugin-simple-import-sort')
+const eslintConfigPrettier = require('eslint-config-prettier')
+const globals = require('globals')
+
+module.exports = tseslint.config(
+    eslint.configs.recommended,
+    pluginPromise.configs['flat/recommended'],
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            ecmaVersion: 2021,
+            globals: {
+                ...globals.browser,
+                ...globals.es2021,
+            },
+            parserOptions: {
+                projectService: true,
+            },
+        },
+        plugins: {
+            n: pluginN,
+            'simple-import-sort': pluginSimpleImportSort,
+        },
+        rules: {
+            // Unnecessary with TS and generates false positives.
+            // https://github.com/typescript-eslint/typescript-eslint/blob/181e705887e9e07f0fa28195644cc94e5b4f039d/docs/linting/Troubleshooting.mdx#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+            'no-undef': 'off',
+            'no-use-before-define': 'off', // Unnecessary with TS and generates false positives.
+            'comma-dangle': ['error', 'always-multiline'],
+            'func-style': ['error', 'declaration'],
+
+            // Allows idiomatic infinite loops, rejects other cases
+            'no-constant-condition': ['error', { checkLoops: false }],
+
+            'no-useless-constructor': 'off',
+            'no-unused-vars': 'off',
+            'no-extra-semi': 'off',
+            'no-trailing-spaces': 'error',
+
+            // Doesn't complain when we have let {a, b} = obj and b is not reassigned,
+            // complains otherwise.
+            'prefer-const': ['error', { destructuring: 'all' }],
+
+            '@typescript-eslint/no-useless-constructor': ['error'],
+            '@typescript-eslint/no-unnecessary-type-assertion': ['error'],
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+            ],
+            '@typescript-eslint/no-unnecessary-condition': ['error'],
+            'simple-import-sort/imports': ['error'],
+            complexity: ['error', { max: 14 }],
+            // To support TS overloads
+            // https://github.com/typescript-eslint/typescript-eslint/blob/6fd476c32c4757cb9f4c442f0cd92875671eed30/packages/eslint-plugin/docs/rules/no-redeclare.md
+            'no-redeclare': 'off',
+            '@typescript-eslint/no-redeclare': ['error'],
+
+            /*
+            @typescript-eslint/recommended-type-checked exceptions
+
+            To be determined if we want to keep these rules disabled long-term, at least some
+            of these look useful, we just need to configure them so they don't generate so many
+            false positives.
+            */
+
+            '@typescript-eslint/require-await': 'off',
+            '@typescript-eslint/unbound-method': 'off',
+
+            // Being able to use and interact with "any" values
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unsafe-member-access': 'off',
+            '@typescript-eslint/no-unsafe-assignment': 'off',
+            '@typescript-eslint/no-unsafe-return': 'off',
+            '@typescript-eslint/no-unsafe-call': 'off',
+            '@typescript-eslint/no-unsafe-argument': 'off',
+
+            // Generates a lof of false positives when it doesn't always understand when a value
+            // can be safely used in a string context. We can try to configure this behavior.
+            '@typescript-eslint/no-base-to-string': 'off',
+
+            // We find it creates more problems than it solves.
+            '@typescript-eslint/no-use-before-define': 'off',
+
+            // Prevents using Big (among others) in string templates
+            '@typescript-eslint/restrict-template-expressions': 'off',
+
+            /*
+            End of @typescript-eslint/recommended-type-checked exceptions
+            */
+
+            'n/handle-callback-err': ['error', '^(err|error)$'],
+            'n/no-callback-literal': 'error',
+            'n/no-deprecated-api': 'error',
+            'n/no-exports-assign': 'error',
+            'n/no-new-require': 'error',
+            'n/no-path-concat': 'error',
+            'n/process-exit-as-throw': 'error',
+        },
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:promise/recommended',
-        'plugin:@typescript-eslint/recommended-type-checked',
-        'prettier',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        projectService: true,
-    },
-    plugins: ['@typescript-eslint', 'n', 'simple-import-sort'],
-    rules: {
-        // Unnecessary with TS and generates false positives.
-        // https://github.com/typescript-eslint/typescript-eslint/blob/181e705887e9e07f0fa28195644cc94e5b4f039d/docs/linting/Troubleshooting.mdx#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-        'no-undef': 'off',
-        'no-use-before-define': 'off', // Unnecessary with TS and generates false positives.
-        'comma-dangle': ['error', 'always-multiline'],
-        'func-style': ['error', 'declaration'],
-
-        // Allows idiomatic infinite loops, rejects other cases
-        'no-constant-condition': ['error', { checkLoops: false }],
-
-        'no-useless-constructor': 'off',
-        'no-unused-vars': 'off',
-        'no-extra-semi': 'off',
-        'no-trailing-spaces': 'error',
-
-        // Doesn't complain when we have let {a, b} = obj and b is not reassigned,
-        // complains otherwise.
-        'prefer-const': ['error', { destructuring: 'all' }],
-
-        '@typescript-eslint/no-useless-constructor': ['error'],
-        '@typescript-eslint/no-unnecessary-type-assertion': ['error'],
-        '@typescript-eslint/no-unused-vars': [
-            'error',
-            { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-        ],
-        '@typescript-eslint/no-unnecessary-condition': ['error'],
-        'simple-import-sort/imports': ['error'],
-        complexity: ['error', { max: 14 }],
-        // To support TS overloads
-        // https://github.com/typescript-eslint/typescript-eslint/blob/6fd476c32c4757cb9f4c442f0cd92875671eed30/packages/eslint-plugin/docs/rules/no-redeclare.md
-        'no-redeclare': 'off',
-        '@typescript-eslint/no-redeclare': ['error'],
-
-        /*
-        @typescript-eslint/recommended-type-checked exceptions
-
-        To be determined if we want to keep these rules disabled long-term, at least some
-        of these look useful, we just need to configure them so they don't generate so many
-        false positives.
-        */
-
-        '@typescript-eslint/require-await': 'off',
-        '@typescript-eslint/unbound-method': 'off',
-
-        // Being able to use and interact with "any" values
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off',
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-unsafe-return': 'off',
-        '@typescript-eslint/no-unsafe-call': 'off',
-        '@typescript-eslint/no-unsafe-argument': 'off',
-
-        // Generates a lof of false positives when it doesn't always understand when a value
-        // can be safely used in a string context. We can try to configure this behavior.
-        '@typescript-eslint/no-base-to-string': 'off',
-
-        // We find it creates more problems than it solves.
-        '@typescript-eslint/no-use-before-define': 'off',
-
-        // Prevents using Big (among others) in string templates
-        '@typescript-eslint/restrict-template-expressions': 'off',
-
-        /*
-        End of @typescript-eslint/recommended-type-checked exceptions
-        */
-
-        'n/handle-callback-err': ['error', '^(err|error)$'],
-        'n/no-callback-literal': 'error',
-        'n/no-deprecated-api': 'error',
-        'n/no-exports-assign': 'error',
-        'n/no-new-require': 'error',
-        'n/no-path-concat': 'error',
-        'n/process-exit-as-throw': 'error',
-    },
-}
+    eslintConfigPrettier,
+)
